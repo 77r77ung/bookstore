c4a2bd72f84af92899efe44f7be853b98b6812f6
77r77ung <2625k@naver.com>
2021-11-30 06:54:23 +0900

    주문 목록까지 띄우기!

--- src/query/orderQuery.js
+++ src/query/orderQuery.js
@@ -3,16 +3,16 @@ exports.addOrderList = 'insert into order_list(order_order_number, book_book_num
 
 exports.minusOrderAmount = 'update book set book_amount = (book_amount - ?) where book_number =?'
 
-exports.selectOrder = 'select * from `order` where `user_user_uid=?` `order` by `order_date`'
+exports.selectOrder = 'select * from `order` where `user_user_uid`=? order by order_date'
 
-exports.orderDetail = 'select * from order where order_unmber=?'
+exports.orderDetail = 'select * from `order` where `order_unmber`=?'
 
 exports.orderBookList = 'select a.order_order_number, a.book_book_number, a.order_amount, b.book_number, b.book_name, b.book_amount, b.book_price, b.book_explain from order_list a, book b where a.book_book_number = b.book_number and a.order_order_number?'
 
-exports.selectOrderList = 'select * from order_list where order_order_number=?'
+exports.selectOrderList = 'select * from `order_list` where `order_order_number`=?'
 
 exports.plusOrder = 'update book set book_amount = (book_amount + ?) where book_number = ?'
 
-exports.deleteOrderList = 'delete from order_list where order_order_number=? and book_book_number=?'
-exports.deleteOrder = 'delete from order where order_number=?'
+exports.deleteOrderList = 'delete from `order_list` where `order_order_number`=? and book_book_number=?'
+exports.deleteOrder = 'delete from `order` where `order_number`=?'
 
